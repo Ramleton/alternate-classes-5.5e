4895f969d10b9581b4aa8cb2ded6c6e172186114
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-05-25 22:27:50 -0400

    feat: update import statement for activityUtils in typhoonStrike and add astralArmor macro for monk subclass

--- macros/classes/monk/mystic-techniques/typhoonStrike.ts
+++ macros/classes/monk/mystic-techniques/typhoonStrike.ts
@@ -1,5 +1,5 @@
 import {
-  activityUtil,
+  activityUtils,
   constants,
   actorUtils,
   dialogUtils,
---
+++ macros/classes/monk/subclasses/astral-warrior/astral-armor.ts
@@ -0,0 +1,50 @@
+import {
+  activityUtils,
+  constants,
+  actorUtils,
+  dialogUtils,
+  effectUtils,
+  genericUtils,
+  itemUtils,
+  socketUtils,
+  tokenUtils,
+  workflowUtils,
+} from 'chris-premades/scripts/utils.js';
+
+async function manifestSave({ trigger: { entity: item }, workflow, ditem }) {
+  const nearbyTargets = tokenUtils.findNearby(workflow.token, 10, 'all', {
+    includeIncapacitated: true,
+    includeToken: false,
+  });
+  if (!nearbyTargets.length) return;
+  const saveActivity = activityUtils.getActivityByIdentifier(item, 'save', {
+    strict: true,
+  });
+  if (!saveActivity) return;
+  const enchantActivity = activityUtils.getActivityByIdentifier(
+    item,
+    'enchant',
+    {
+      strict: true,
+    },
+  );
+  if (!enchantActivity) return;
+  await workflowUtils.syntheticActivityRoll(saveActivity, nearbyTargets);
+  await workflowUtils.syntheticActivityRoll(enchantActivity);
+}
+
+export const ac55eAstralArmor = {
+  name: 'Astral Armor',
+  version: '1.3.141',
+  rules: 'modern',
+  midi: {
+    item: [
+      {
+        pass: 'rollFinished',
+        macro: manifestSave,
+        priority: 50,
+        activities: ['manifest'],
+      },
+    ],
+  },
+};
