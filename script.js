923355035d085ba0561cad5d81cab356096273e8
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-05-29 17:56:31 -0400

    Alternate Monk Complete
    - Added Way of the Radiance
    - Added Way of the Reaper
    - Added Way of the Rising Dragon
    - Added Way of the Shadow Arts
    - Added Way of the Shining Steel
    - Added Way of the Wu Jen
    - Reworked Mystic Techniques to handle all subclasses

--- script.js
+++ script.js
@@ -1,10 +1,63 @@
-// Adds the class feature types from LaserLlama's Alternate Classes
 Hooks.once('init', () => {
+  // Adds the class feature types from LaserLlama's Alternate Classes
   CONFIG.DND5E.featureTypes.class.subtypes.knack = 'Knack';
   CONFIG.DND5E.featureTypes.class.subtypes.deviousExploit = 'Devious Exploit';
   CONFIG.DND5E.featureTypes.class.subtypes.savageExploit = 'Savage Exploit';
   CONFIG.DND5E.featureTypes.class.subtypes.martialExploit = 'Martial Exploit';
   CONFIG.DND5E.featureTypes.class.subtypes.mysticTechnique = 'Mystic Technique';
+  console.log('Alternate Classes 5e | Initialized class feature types');
+
+  class WuJenSpellcasting extends dnd5e
+    .dataModels
+    .spellcasting
+    .SingleLevelSpellcasting {
+    /** @override */
+    static get TYPE() {
+      return 'wuJen';
+    }
+  }
+
+  dnd5e.dataModels.spellcasting.SpellcastingModel.TYPES.wuJen
+    = WuJenSpellcasting;
+
+  CONFIG.DND5E.spellcastingTypes.wuJen = WuJenSpellcasting;
+
+  // Add the Wu Jen Spellcasting type
+  CONFIG.DND5E.spellcasting.wuJen = {
+    label: 'Wu Jen Spellcasting',
+    type: 'single',
+    order: 15,
+    cantrips: true,
+    prepares: false,
+    img: 'icons/magic/air/air-smoke-casting.webp',
+    table: {
+      3: { slots: 1, level: 1 },
+      4: { slots: 2, level: 1 },
+      7: { slots: 2, level: 2 },
+      13: { slots: 2, level: 3 },
+      19: { slots: 2, level: 4 },
+    },
+    progression: {
+      wuJen: {
+        label: 'Wu Jen Spellcasting',
+        divisor: 1,
+        roundUp: true,
+      },
+    },
+    exclusive: {
+      slots: false,
+      spells: false,
+    },
+  };
+  console.log('Alternate Classes 5e | Initialized Wu Jen Spellcasting');
+
+  /**
+   * Add Rest Recovery for Alternate Classes Spellcasting Types
+   */
+  CONFIG.DND5E.restTypes.short.recoverSpellSlotTypes.add('wuJen');
+  CONFIG.DND5E.restTypes.long.recoverSpellSlotTypes.add('wuJen');
+  console.log('Alternate Classes 5e | \
+    Initialized rest recovery for Alternate Classes spellcasting types');
 
   /**
    * API for Alternate Classes 5e
@@ -44,4 +97,5 @@ Hooks.once('init', () => {
         .isMartialArtsAttack({ workflow });
     },
   };
+  console.log('Alternate Classes 5e | Initialized API');
 });
