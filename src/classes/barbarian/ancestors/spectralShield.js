9a59348a2b55cbdd5bd6e7b6ede45d22b70e26d3
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-06-06 01:31:52 -0400

    feat: add Chivalric Mark with pre, during, and post workflow management

--- src/classes/barbarian/ancestors/spectralShield.js
+++ src/classes/barbarian/ancestors/spectralShield.js
@@ -1,41 +1,82 @@
 async function shieldHelper(token, sourceToken, targetToken, ditem) {
-	if (actorUtils.hasUsedReaction(token.actor)) return;
-	let rageEffect = effectUtils.getEffectByIdentifier(token.actor, 'ac55eRage');
-	if (!rageEffect) return;
-	let spectralShield = itemUtils.getItemByIdentifier(token.actor, 'ac55eSpectralShield');
-	if (!spectralShield) return;
-	let selection = await dialogUtils.confirm(spectralShield.name, genericUtils.format('CHRISPREMADES.Macros.SpiritShield.Damage', {item: spectralShield.name, name: targetToken.document.name}), {userId: socketUtils.firstOwner(token.actor, true)});
-	if (!selection) return;
-	let result = await workflowUtils.syntheticItemRoll(spectralShield, [token], {consumeResources: true, userId: socketUtils.firstOwner(token.actor, true)});
-	workflowUtils.modifyDamageAppliedFlat(ditem, -result.damageRolls[0].total);
-	let vengefulAncestors = itemUtils.getItemByIdentifier(token.actor, 'ac55eVengefulAncestors');
-	if (vengefulAncestors) {
-		let activity = activityUtils.getActivityByIdentifier(vengefulAncestors, 'damage', {strict: true});
-		if (!activity) return true;
-		let featureData = genericUtils.duplicate(vengefulAncestors.toObject());
-		featureData.system.activities[activity.id].damage.parts[0].bonus = result.damageRolls[0].total;
-		await workflowUtils.syntheticItemDataRoll(featureData, token.actor, [sourceToken]);
-	}
-	return true;
+  if (actorUtils.hasUsedReaction(token.actor)) return;
+  let rageEffect = effectUtils.getEffectByIdentifier(token.actor, 'ac55eRage');
+  if (!rageEffect) return;
+  let spectralShield = itemUtils.getItemByIdentifier(
+    token.actor,
+    'ac55eSpectralShield',
+  );
+  if (!spectralShield) return;
+  let selection = await dialogUtils.confirm(
+    spectralShield.name,
+    genericUtils.format(
+      'CHRISPREMADES.Macros.SpiritShield.Damage',
+      { item: spectralShield.name, name: targetToken.document.name },
+    ),
+    { userId: socketUtils.firstOwner(token.actor, true) },
+  );
+  if (!selection) return;
+  let result = await workflowUtils.syntheticItemRoll(
+    spectralShield,
+    [token],
+    {
+      consumeResources: true,
+      userId: socketUtils.firstOwner(token.actor, true),
+    },
+  );
+  workflowUtils.modifyDamageAppliedFlat(ditem, -result.damageRolls[0].total);
+  let vengefulAncestors = itemUtils.getItemByIdentifier(
+    token.actor,
+    'ac55eVengefulAncestors',
+  );
+  if (vengefulAncestors) {
+    let activity = activityUtils.getActivityByIdentifier(
+      vengefulAncestors,
+      'damage',
+      { strict: true },
+    );
+    if (!activity) return true;
+    let featureData = genericUtils.duplicate(vengefulAncestors.toObject());
+    featureData
+      .system
+      .activities[activity.id]
+      .damage
+      .parts[0]
+      .bonus = result.damageRolls[0].total;
+    await workflowUtils.syntheticItemDataRoll(
+      featureData,
+      token.actor,
+      [sourceToken],
+    );
+  }
+  return true;
 }
-async function damageApplication({trigger: {sourceToken, targetToken}, ditem}) {
-	let nearbyTokens = tokenUtils.findNearby(targetToken, 30, 'ally', {includeIncapacitated: false, includeToken: true});
-	for (let i of nearbyTokens) {
-		let shielded = await shieldHelper(i, sourceToken, targetToken, ditem);
-		if (shielded) break;
-	}
+async function damageApplication({
+  trigger: { sourceToken, targetToken },
+  ditem,
+}) {
+  let nearbyTokens = tokenUtils.findNearby(
+    targetToken,
+    30,
+    'ally',
+    { includeIncapacitated: false, includeToken: true },
+  );
+  for (let i of nearbyTokens) {
+    let shielded = await shieldHelper(i, sourceToken, targetToken, ditem);
+    if (shielded) break;
+  }
 }
 export const ac55eSpectralShield = {
-	name: 'Spectral Shield',
-	version: '1.0.0',
-	rules: 'modern',
-	midi: {
-		actor: [
-			{
-				pass: 'sceneApplyDamage',
-				macro: damageApplication,
-				priority: 250
-			}
-		]
-	}
+  name: 'Spectral Shield',
+  version: '1.0.0',
+  rules: 'modern',
+  midi: {
+    actor: [
+      {
+        pass: 'sceneApplyDamage',
+        macro: damageApplication,
+        priority: 250,
+      },
+    ],
+  },
 };
---
+++ src/classes/fighter/subclasses/knight-errant/chivalricMarkApply.ts
@@ -0,0 +1,76 @@
+import { Workflow } from '@midi-qol/types/module/Workflow';
+import {
+  activityUtils,
+  dialogUtils,
+  socketUtils,
+  workflowUtils,
+} from 'chrisPremades';
+import {
+  AlternateClasses55eAPI,
+} from '../../../../types/alternate-classes-55e';
+
+async function pre(
+  item,
+  workflow: Workflow,
+): Promise<boolean> {
+  if (!workflow.hitTargets.size) return false;
+  if (!item.system.uses.value) return false;
+  const selection = await dialogUtils.confirmUseItem(item, {
+    userId: socketUtils.firstOwner(item.actor, true),
+  });
+  return selection;
+}
+
+async function during(
+  item,
+  workflow: Workflow,
+): Promise<boolean> {
+  const useActivity = activityUtils.getActivityByIdentifier(item, 'use', {
+    strict: true,
+  });
+  if (!useActivity) return false;
+  await workflowUtils.syntheticActivityRoll(
+    useActivity,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [workflow.hitTargets.first() as any],
+    {
+      consumeResources: true,
+    },
+  );
+  return true;
+}
+
+async function post(
+  item,
+): Promise<void> {
+  await item.update({ 'system.uses.spent': item.system.uses.spent + 1 });
+}
+
+async function workflow({
+  trigger: { entity: item },
+  workflow,
+}) {
+  const altClassesModule = game.modules
+    ?.get('alternate-classes-55e') as AlternateClasses55eAPI | undefined;
+  if (!altClassesModule) return;
+  const res1 = await pre(item, workflow);
+  if (!res1) return;
+  const res2 = await during(item, workflow);
+  if (!res2) return;
+  await post(item);
+}
+
+export const ac55eExploitChivalricMarkApply = {
+  name: 'Chivalric Mark: Apply',
+  version: '1.3.141',
+  rules: 'modern',
+  midi: {
+    actor: [
+      {
+        pass: 'attackRollComplete',
+        macro: workflow,
+        priority: 100,
+      },
+    ],
+  },
+};
