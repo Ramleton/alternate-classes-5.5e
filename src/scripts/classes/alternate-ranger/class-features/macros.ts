02854bf9ae78826c63d9ccf39dcf103278f04ea6
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-06-27 14:52:37 -0400

    feat(ranger): add Ranger's Quarry macro and integrate into class macros

---
+++ src/scripts/classes/alternate-ranger/class-features/macros.ts
@@ -0,0 +1,6 @@
+import CPRMacro from '../../../../types/chris-premades/macro.js';
+import rangersQuarry from './rangersQuarry.js';
+
+const macros: CPRMacro[] = [rangersQuarry];
+
+export default macros;
