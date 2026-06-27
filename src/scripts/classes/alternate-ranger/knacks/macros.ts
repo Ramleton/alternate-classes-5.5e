052981c3ee1114aa4f27208baafe0dd7e73b33a9
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-06-27 18:07:54 -0400

    feat(ranger): add Herbalist II macro and integrate into class macros

--- src/scripts/classes/alternate-ranger/knacks/macros.ts
+++ src/scripts/classes/alternate-ranger/knacks/macros.ts
@@ -1,6 +1,7 @@
 import CPRMacro from '../../../../types/chris-premades/macro.js';
+import herbalistII from './herbalistII.js';
 import slayerIII from './slayerIII.js';
 
-const macros: CPRMacro[] = [slayerIII];
+const macros: CPRMacro[] = [herbalistII, slayerIII];
 
 export default macros;
