423d66ce17472369282be9bebbecbd33fa19f711
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-06-30 19:27:24 -0400

    feat(ranger): implement Ruthless Counter macro for Monster Slayer subclass and update macro list

--- src/scripts/classes/alternate-ranger/subclasses/monster-slayer/macros.ts
+++ src/scripts/classes/alternate-ranger/subclasses/monster-slayer/macros.ts
@@ -2,7 +2,8 @@ import CPRMacro from 'chris-premades/macro.js';
 import ironFocus from './ironFocus.js';
 import knowledgeOfTheHunt from './knowledgeOfTheHunt.js';
 import monstersNemesis from './monstersNemesis.js';
+import ruthlessCounter from './ruthlessCounter.js';
 
-const macros: CPRMacro[] = [ironFocus, knowledgeOfTheHunt, monstersNemesis];
+const macros: CPRMacro[] = [ironFocus, knowledgeOfTheHunt, monstersNemesis, ruthlessCounter];
 
 export default macros;
