93e1fc37687c7ad7a444c36a667841f4f446eea2
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-06-29 23:41:26 -0400

    feat(ranger): add Mirthful Step macro to Fey Wanderer subclass

--- src/scripts/classes/alternate-ranger/subclasses/fey-wanderer/macros.ts
+++ src/scripts/classes/alternate-ranger/subclasses/fey-wanderer/macros.ts
@@ -1,7 +1,8 @@
 import CPRMacro from 'chris-premades/macro.js';
 import beguilingStrikes from './beguilingStrikes.js';
+import mirthfulStep from './mirthfulStep.js';
 import whimsicalWard from './whimsicalWard.js';
 
-const macros: CPRMacro[] = [beguilingStrikes, whimsicalWard];
+const macros: CPRMacro[] = [beguilingStrikes, mirthfulStep, whimsicalWard];
 
 export default macros;
