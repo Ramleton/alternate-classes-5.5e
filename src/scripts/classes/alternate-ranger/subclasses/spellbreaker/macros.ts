a224375722df626614571f9fbd53a5a7ae6d6883
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-01 15:37:10 -0400

    feat(ranger): add Spellbane macro with Counterspell functionality and update macros list

--- src/scripts/classes/alternate-ranger/subclasses/spellbreaker/macros.ts
+++ src/scripts/classes/alternate-ranger/subclasses/spellbreaker/macros.ts
@@ -1,7 +1,8 @@
 import CPRMacro from 'chris-premades/macro.js';
 import arcaneDefense from './arcaneDefense.js';
 import mageBreaker from './mageBreaker.js';
+import spellbane from './spellbane.js';
 
-const macros: CPRMacro[] = [arcaneDefense, mageBreaker];
+const macros: CPRMacro[] = [arcaneDefense, mageBreaker, spellbane];
 
 export default macros;
