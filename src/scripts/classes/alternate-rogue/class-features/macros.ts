397771c0bf5e00ec3e894f727c927c536b6825d7
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-02 22:47:56 -0400

    feat: add Slippery Mind macro and include it in the rogue class features

--- src/scripts/classes/alternate-rogue/class-features/macros.ts
+++ src/scripts/classes/alternate-rogue/class-features/macros.ts
@@ -1,8 +1,9 @@
 import CPRMacro from 'chris-premades/macro.js';
 import cunningStrike from './cunningStrike.js';
+import slipperyMind from './slipperyMind.js';
 import sneakAttack from './sneakAttack.js';
 import uncannyDodge from './uncannyDodge.js';
 
-const macros: CPRMacro[] = [cunningStrike, sneakAttack, uncannyDodge];
+const macros: CPRMacro[] = [cunningStrike, slipperyMind, sneakAttack, uncannyDodge];
 
 export default macros;
---
+++ src/scripts/classes/alternate-rogue/class-features/slipperyMind.ts
@@ -0,0 +1,31 @@
+import CPRMacro, { MacroFunction } from 'chris-premades/macro.js';
+import { getAlternateMartialExploitDie } from 'exploits/utils.js';
+
+const bonus: MacroFunction = async ({ trigger: { entity, roll, saveId } }) => {
+  if (!['int', 'wis', 'cha'].some((id) => id === saveId)) return;
+  const {
+    utils: { rollUtils },
+  } = chrisPremades;
+  const feat = entity as Item<'feat'>;
+  const exploitDie = getAlternateMartialExploitDie(feat);
+  if (!exploitDie) return;
+  const formula = `1d${exploitDie.faces}`;
+  return await rollUtils.addToRoll(roll, formula);
+};
+
+const macro: CPRMacro = {
+  identifier: 'ac55eSlipperyMind',
+  name: 'Slippery Mind',
+  source: 'Alternate Classes 5.5e',
+  version: '1.0.0',
+  rules: 'modern',
+  save: [
+    {
+      pass: 'bonus',
+      macro: bonus,
+      priority: 0,
+    },
+  ],
+};
+
+export default macro;
