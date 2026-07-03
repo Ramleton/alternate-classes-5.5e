084f9623afa9974830da5d8f7514fafce2e14d03
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-03 01:29:39 -0400

    feat: implement autoCritical functionality in Assassinate macro

--- src/scripts/classes/alternate-rogue/subclasses/assassin/assassinate.ts
+++ src/scripts/classes/alternate-rogue/subclasses/assassin/assassinate.ts
@@ -1,9 +1,37 @@
-import CPRMacro, { MacroFunction } from 'chris-premades/macro.js';
+import CPRMacro, {
+  MacroFunction,
+  MidiMacroFunction,
+} from 'chris-premades/macro.js';
 import {
   getAlternateMartialExploitDie,
   getAltMartialExploitsRemaining,
   spendAlternateMartialExploitUses,
 } from 'exploits/utils.js';
+
+const autoCritical: MidiMacroFunction = async ({
+  trigger: { entity, token },
+  workflow,
+}) => {
+  const feat = entity as Item<'feat'>;
+  if (!feat.actor) return;
+  if (!feat.actor.flags['alternate-classes-55e']?.macros?.['sneak-attack'])
+    return;
+  if (!workflow.hitTargets.size) return;
+  const {
+    utils: { constants, workflowUtils },
+  } = chrisPremades;
+  const actionType = workflowUtils.getActionType(workflow);
+  if (!constants.weaponAttacks.some((type) => type === actionType)) return;
+  const target = workflow.targets.first() as Token;
+  const isTargetIncapacitated = target.actor!.statuses.has('incapacitated');
+  const isTargetSurprised = target.actor!.statuses.has('surprised');
+  if (!isTargetIncapacitated && !isTargetSurprised) return;
+  workflow.isCritical = true;
+  await ChatMessage.create({
+    content: `${feat.name}: Critical Hit`,
+    speaker: ChatMessage.getSpeaker({ token: token }),
+  });
+};
 
 const prompt: MacroFunction = async ({ trigger: { entity, token } }) => {
   const feat = entity as Item<'feat'>;
@@ -37,6 +65,15 @@ const macro: CPRMacro = {
   source: 'Alternate Classes 5.5e',
   version: '1.0.0',
   rules: 'modern',
+  midi: {
+    actor: [
+      {
+        pass: 'attackRollComplete',
+        macro: autoCritical,
+        priority: 100,
+      },
+    ],
+  },
   combat: [
     {
       pass: 'combatStart',
