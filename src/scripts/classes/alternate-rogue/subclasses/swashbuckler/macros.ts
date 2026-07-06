82583f6c1929f822f4a90937bcc1f6061e2eeb94
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-06 17:19:17 -0400

    feat: Add Elegant Warrior macro for Swashbuckler subclass

--- src/scripts/classes/alternate-rogue/subclasses/swashbuckler/macros.ts
+++ src/scripts/classes/alternate-rogue/subclasses/swashbuckler/macros.ts
@@ -1,7 +1,8 @@
 import CPRMacro from 'chris-premades/macro.js';
+import elegantWarrior from './elegantWarrior.js';
 import panache from './panache.js';
 import relentlessSwagger from './relentlessSwagger.js';
 
-const macros: CPRMacro[] = [panache, relentlessSwagger];
+const macros: CPRMacro[] = [elegantWarrior, panache, relentlessSwagger];
 
 export default macros;
