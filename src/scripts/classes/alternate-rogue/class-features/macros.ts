ac45b85e037e7b2c6606373a5ccaeed4d9dd6eab
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-08 14:24:29 -0400

    feat(exploits): integrate spendExploitUses in Devious Exploits macro and update macros list

--- src/scripts/classes/alternate-rogue/class-features/macros.ts
+++ src/scripts/classes/alternate-rogue/class-features/macros.ts
@@ -1,9 +1,16 @@
 import CPRMacro from 'chris-premades/macro.js';
 import cunningStrike from './cunningStrike.js';
+import deviousExploits from './deviousExploits.js';
 import slipperyMind from './slipperyMind.js';
 import sneakAttack from './sneakAttack.js';
 import uncannyDodge from './uncannyDodge.js';
 
-const macros: CPRMacro[] = [cunningStrike, slipperyMind, sneakAttack, uncannyDodge];
+const macros: CPRMacro[] = [
+  cunningStrike,
+  deviousExploits,
+  slipperyMind,
+  sneakAttack,
+  uncannyDodge,
+];
 
 export default macros;
