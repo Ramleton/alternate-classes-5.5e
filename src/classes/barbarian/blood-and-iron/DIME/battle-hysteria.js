62efac3fdfc70e1afec2a0d61c7dc0fbe21a28db
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-16 05:43:08 -0400

    Remove obsolete barbarian DIME scripts

--- src/classes/barbarian/blood-and-iron/DIME/battle-hysteria.js
+++
@@ -1,6 +0,0 @@
-if (workflow.hitTargets.size > 0 &&
-	workflow.activity.ability === 'str' &&
-	workflow.actor.effects.some(e => e.name.toLowerCase() === 'reckless attack')
-) {
-  return MidiQOL.completeActivityUse(macroActivity);
-}
