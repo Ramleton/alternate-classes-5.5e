e479dca6d2fd49c41073423ea57ff98a622e53bc
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-24 00:10:58 -0400

    feat: add Empowered Strike mystic technique and update macros

--- src/scripts/classes/alternate-monk/mystic-techniques/macros.ts
+++ src/scripts/classes/alternate-monk/mystic-techniques/macros.ts
@@ -3,12 +3,14 @@ import arrestingStrike from './arrestingStrike.js';
 import cripplingStrike from './cripplingStrike.js';
 import dazingStrike from './dazingStrike.js';
 import deflectStrike from './deflectStrike.js';
+import empoweredStrike from './empoweredStrike.js';
 
 const macros: CPRMacro[] = [
   arrestingStrike,
   cripplingStrike,
   dazingStrike,
   deflectStrike,
+  empoweredStrike,
 ];
 
 export default macros;
