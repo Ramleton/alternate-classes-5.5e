7adbb1109c5f9618d0f9968b2503d540f50574e2
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-25 00:10:49 -0400

    feat: add Deflect Missile mystic technique and include in macros

--- src/scripts/classes/alternate-monk/mystic-techniques/macros.ts
+++ src/scripts/classes/alternate-monk/mystic-techniques/macros.ts
@@ -2,6 +2,7 @@ import CPRMacro from 'chris-premades/macro.js';
 import arrestingStrike from './arrestingStrike.js';
 import cripplingStrike from './cripplingStrike.js';
 import dazingStrike from './dazingStrike.js';
+import deflectMissile from './deflectMissile.js';
 import deflectStrike from './deflectStrike.js';
 import empoweredStrike from './empoweredStrike.js';
 import enhancedGrip from './enhancedGrip.js';
@@ -12,6 +13,7 @@ const macros: CPRMacro[] = [
   arrestingStrike,
   cripplingStrike,
   dazingStrike,
+  deflectMissile,
   deflectStrike,
   empoweredStrike,
   enhancedGrip,
