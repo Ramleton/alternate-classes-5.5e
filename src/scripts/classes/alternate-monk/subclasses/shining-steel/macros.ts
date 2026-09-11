c9e7ec42d7525c348a1d70fd0b7f2dc64292ba1e
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-09-11 19:40:22 -0400

    feat: remove deadlyPerfection macro from Shining Steel subclass

--- src/scripts/classes/alternate-monk/subclasses/shining-steel/macros.ts
+++ src/scripts/classes/alternate-monk/subclasses/shining-steel/macros.ts
@@ -1,7 +1,6 @@
 import CPRMacro from 'chris-premades/macro.js';
-import deadlyPerfection from './deadlyPerfection.js';
 import masterOfShiningSteel from './masterOfShiningSteel.js';
 
-const macros: CPRMacro[] = [deadlyPerfection, masterOfShiningSteel];
+const macros: CPRMacro[] = [masterOfShiningSteel];
 
 export default macros;
