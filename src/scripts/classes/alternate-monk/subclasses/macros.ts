0db35170278c091b9ecd0bf2f13ff6fd2a0d773c
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-09-11 18:28:02 -0400

    feat: add Open Hand subclass macros and integrate Ebb and Flow functionality

--- src/scripts/classes/alternate-monk/subclasses/macros.ts
+++ src/scripts/classes/alternate-monk/subclasses/macros.ts
@@ -2,7 +2,13 @@ import CPRMacro from 'chris-premades/macro.js';
 import astralWarrior from './astral-warrior/macros.js';
 import drunkenFist from './drunken-fist/macros.js';
 import harmony from './harmony/macros.js';
+import openHand from './open-hand/macros.js';
 
-const macros: CPRMacro[] = [...astralWarrior, ...drunkenFist, ...harmony];
+const macros: CPRMacro[] = [
+  ...astralWarrior,
+  ...drunkenFist,
+  ...harmony,
+  ...openHand,
+];
 
 export default macros;
---
+++ src/scripts/classes/alternate-monk/subclasses/open-hand/macros.ts
@@ -0,0 +1,6 @@
+import CPRMacro from 'chris-premades/macro.js';
+import ebbAndFlow from './ebbAndFlow.js';
+
+const macros: CPRMacro[] = [ebbAndFlow];
+
+export default macros;
