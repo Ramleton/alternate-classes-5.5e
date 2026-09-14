113ccf3960c96202f7aa1db527bab8ce7f7e46ed
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-09-14 18:59:41 -0400

    feat: add Eldritch Blast macro and handler factory for Alternate Warlock class

---
+++ src/scripts/classes/alternate-warlock/class-features/macros.ts
@@ -0,0 +1,6 @@
+import CPRMacro from 'chris-premades/macro.js';
+import eldritchBlast from './eldritchBlast.js';
+
+const macros: CPRMacro[] = [eldritchBlast];
+
+export default macros;
--- src/scripts/classes/alternate-warlock/macros.ts
+++ src/scripts/classes/alternate-warlock/macros.ts
@@ -1,6 +1,7 @@
 import CPRMacro from 'chris-premades/macro.js';
+import classFeatures from './class-features/macros.js';
 import eldritchInvocations from './eldritch-invocations/macros.js';
 
-const macros: CPRMacro[] = [...eldritchInvocations];
+const macros: CPRMacro[] = [...classFeatures, ...eldritchInvocations];
 
 export default macros;
